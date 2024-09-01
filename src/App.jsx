15ca6a944e332ee8b58a7aad40f31c28ca01140a
md al rasel <mdalrasel96@gmail.com>
2024-09-01 16:07:03 +0600

    newUser create

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,35 @@ function App() {
 
   const [users, setUsers] = useState([])
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch('http://localhost:5000/users')
-    .then(res => res.json())
-    .then(data => setUsers(data))
-  },[]);
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
 
-  const handleAddUsers= e =>{
-e.preventDefault()
+  const handleAddUsers = e => {
+    e.preventDefault()
 
-const form = e.target
-const name = form.name.value;
-const email =form.email.value;
-const user ={name, email}
-console.log(user)
+    const form = e.target
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email }
+    console.log(user)
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        const newUser = [...users, data]
+        setUsers(newUser);
+        form.reset()
+
+      })
   }
 
   return (
@@ -28,18 +43,18 @@ console.log(user)
       <h1>Users Management</h1>
       <p>Numbers of Users: {users.length}</p>
       <form onSubmit={handleAddUsers}>
-        <input type="text" name='name' /> <br /> 
-        <input type="text" name='email' /><br /> 
+        <input type="text" name='name' required/> <br />
+        <input type="email" name='email' required/><br />
         <input type="submit" value='Add User' />
       </form>
       <div>
         {
-          users.map(user =><p>{user.id} : {user.name} {user.email}</p>
+          users.map(user => <p>{user.id} : {user.name} {user.email}</p>
 
           )
         }
       </div>
-     
+
     </>
   )
 }
